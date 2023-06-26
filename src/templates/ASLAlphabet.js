03b4a6bfba43a7092470f902a078ffa3bfd70e99
Eduardo Jami <eduardolalojami@gmail.com>
2023-06-25 19:10:12 -0500

    feat: implement Camera

--- src/templates/ASLAlphabet.js
+++ src/templates/ASLAlphabet.js
@@ -1,10 +1,12 @@
 import Navbar from "@components/Navbar";
+import Camara from "@components/Camara";
 
 const Alphabet = () => {
   return (
     <div>
       <Navbar />
       <h1>This is the alphabet</h1>
+      <Camara />
     </div>
   );
 }
